1e5891ce0c1adfe960cc95d2f3f0cacb6f1f674a
Koromonnnnnnnn <764363@dpsk12.net>
2024-01-10 08:20:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,17 @@ screen = pygame.display.set_mode((800, 800))
 screen.fill((0, 0, 0))
 clock = pygame.time.Clock()
 gameover = False
+
+goldfish = pygame.image.load('goldfish.png')
+goldfish = pygame.transform.scale(goldfish, (100, 100))
+
+while not gameover:  # GAME LOOP############################################################
+    clock.tick(60)
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+            
+
+    pygame.display.flip()
+    screen.fill((0, 0, 0))
