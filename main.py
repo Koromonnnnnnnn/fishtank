4108852c68cb69e2f5961a1602ae2648b7db8d7f
Koromonnnnnnnn <764363@dpsk12.net>
2024-01-10 09:16:27 -0700

    b

--- main.py
+++ main.py
@@ -11,17 +11,30 @@ screen.fill((0, 0, 0))
 clock = pygame.time.Clock()
 gameover = False
 
+
+eel = pygame.image.load('eel.png')
+eel = pygame.transform.scale(eel, (100, 100))
+
 goldfish = pygame.image.load('goldfish.png')
 goldfish = pygame.transform.scale(goldfish, (100, 100))
 
-while not gameover:  # GAME LOOP############################################################
+clownfish = pygame.image.load('clownfish.png')
+clownfish = pygame.transform.scale(clownfish, (100, 100))
+
+starfish = pygame.image.load('starfish.png')
+starfish = pygame.transform.scale(starfish, (100, 100))
+
+while not gameover:  
     clock.tick(60)
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
-            
+         
+    screen.blit(eel, (500, 500))
     screen.blit(goldfish, (400,400))
+    screen.blit(clownfish, (200, 200))
+    screen.blit(starfish, (100, 100))
     
     pygame.display.flip()
     screen.fill((0, 0, 0))
